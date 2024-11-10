7829ee35105c88c0aab34758bb01e8b833ca026d
BiyonFernando <biyon@wso2.com>
2024-11-10 13:38:41 +0530

    test petsc4py

--- app.py
+++ app.py
@@ -3,6 +3,5 @@ print("Test Image")
 # print("Current Location :", os.getcwd())
 # print(os.listdir())
 
-while 1:
-    print("Test Image")
-    print("Hi")
+import petsc4py
+print(dir(petsc4py.PETSc))
