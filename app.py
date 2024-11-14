dbbc949bba0ea499e1dafddea612242989c5bcd3
BiyonFernando <biyon@wso2.com>
2024-11-14 14:43:04 +0530

    test dolfinx

--- app.py
+++ app.py
@@ -2,11 +2,7 @@
 print("Test Image")
 # print("Current Location :", os.getcwd())
 # print(os.listdir())
-<<<<<<< HEAD
-import dolfinx
-=======
-from dolfinx.fem import *
->>>>>>> 9673c02b76809ed3177da42bb75a495d324e109b
 
+import dolfinx
 
 print(dir(dolfinx.fem))
