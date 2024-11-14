00b68d1575e878df8e5e20ed413db5b2f3b360d7
SupunDK <supun2110@gmail.com>
2024-11-14 16:31:20 +0530

    check the availability of FunctionSpace_float64 and TestFunction packages

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ print("Test Image")
 
 import dolfinx
 
-packages_to_check = ["FiniteElement", "FunctionSpace", "Expression", "Function", "TestFunctions", "DirichletBC", "solve"]
+packages_to_check = ["FiniteElement", "FunctionSpace", "FunctionSpace_float64", "Expression", "Function", "TestFunctions", "TestFunction", "DirichletBC", "solve"]
 available_package_list = dir(dolfinx.fem)
 
 for package in packages_to_check:
     if package in available_package_list:
         print(f"{package} is available")
     else:
-        print(f"{package} is not available")
+        print(f"[ERROR] {package} is not available")
