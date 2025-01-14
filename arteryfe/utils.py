fc3162c987364dc9cff297cd96a68f59c0999192
BiyonFernando <biyon@wso2.com>
2025-01-14 16:45:24 +0530

    Save numpy arrays in .npy formate

--- arteryfe/utils.py
+++ arteryfe/utils.py
@@ -289,6 +289,9 @@ def plot_matrix(t, x, M, label, output):
     print('Saving matrix to %s.' % (output))
     plt.savefig(output)
 
+    np.save(output[:-4]+'_M.npy',M)
+    np.save(output[:-4]+'_x.npy',x)
+    np.save(output[:-4]+'_t.npy',t)
 
 def is_near(a, b, tol=1.e-11, reltol=1.e-10):
     """
