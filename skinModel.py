fix issue with out_flux calculation

skinModel.py
@@ -277,7 +277,7 @@ def model(parameters, skin_model_const_params, in_flux = 1):
             xlayer_voxels_in_flux = xlayer_in_flux * norm * skin_model_const_params.get_voxel_yz_area()
 
             # calculating the flux coming out of each voxel in this x layer
-            xlayer_voxels_out_flux = xlayer_voxels_in_flux * (1 - xlayer_voxels_mu * skin_model_const_params.get_mean_light_path_distances()[i])
+            xlayer_voxels_out_flux = xlayer_voxels_in_flux * (1 - xlayer_voxels_mu * skin_model_const_params.get_ln10() * skin_model_const_params.get_mean_light_path_distances()[i])
 
             # calculating the total flux coming out of this x layer
             xlayer_out_flux = np.sum(xlayer_voxels_out_flux)